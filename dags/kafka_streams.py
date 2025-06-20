963e99b2df633fa010fbf508976643b56fb1a8e3
samwanyua <samexample8@gmail.com>
2025-06-20 18:44:41 +0300

    format fetched data from the API

--- dags/kafka_streams.py
+++ dags/kafka_streams.py
@@ -1,21 +1,50 @@
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+import uuid
+
 
 default_args = {
     'owner': 'wanyua',
     'start_date': datetime(2025, 6, 20, 18,00)
 }
 
-def stream_data():
+def get_data():
     import json
     import requests
 
     res = requests.get("https://randomuser.me/api/")
     res=res.json()
     res = res['results'][0]
+
+    return res
+
+def format_data(res):
+    data = {}
+    location = res['location']
+    data['id'] = str(uuid.uuid4())
+    data['first_name'] = res['name']['first']
+    data['last_name'] = res['name']['last']
+    data['gender'] = res['gender']
+    data['address'] = f"{str(location['street']['number'])} {location['street']['name']}"\
+                        f"{location['city']}, {location['state']}, {location['country']}"
+    data['postcode'] = location['postcode']
+    data['email'] = res['email']
+    data['username'] = res['login']['username']
+    data['dob'] = res['dob']
+    data['registered_date'] = res['registered']['date']
+    data['phone'] =res['phone']
+    data['picture'] = res['picture']['medium']
+
+    return data
+
+
+
+def stream_data():
+    import json
+    res = get_data()
+    res = format_data(res)
     print(json.dumps(res, indent=3))
-    
 
 with DAG('user_automation',
             default_args=default_args,

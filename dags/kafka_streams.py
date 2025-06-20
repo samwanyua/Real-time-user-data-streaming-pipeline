01d93a1bc78569bd717e0bcf7ff7d0a67e50d725
samwanyua <samexample8@gmail.com>
2025-06-20 18:09:24 +0300

    fetch data from the API

--- dags/kafka_streams.py
+++ dags/kafka_streams.py
@@ -6,3 +6,25 @@ default_args = {
     'owner': 'wanyua',
     'start_date': datetime(2025, 6, 20, 18,00)
 }
+
+def stream_data():
+    import json
+    import requests
+
+    res = requests.get("https://randomuser.me/api/")
+    print(res.json())
+    
+
+with DAG('user_automation',
+            default_args=default_args,
+            schedule_interval='@daily',
+            catchup=False
+         
+         ) as dag:
+    streaming_task = PythonOperator(
+        task_id = 'stream_data_from_api',
+        python_callable=stream_data,
+    )
+    
+
+stream_data()

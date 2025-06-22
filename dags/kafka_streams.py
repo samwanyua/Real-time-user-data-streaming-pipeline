cc3a0b7231495366c123b0359bdf2ef69c29f9ab
samwanyua <samexample8@gmail.com>
2025-06-22 19:51:07 +0300

    Add github actions Continous Integration Pipeline

--- dags/kafka_streams.py
+++ dags/kafka_streams.py
@@ -2,22 +2,22 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 import uuid
+import json
+import requests
+from kafka import KafkaProducer
 
 
 default_args = {
     'owner': 'wanyua',
-    'start_date': datetime(2025, 6, 20, 18,00)
+    'start_date': datetime(2025, 6, 20, 18, 0)
 }
 
+
 def get_data():
-    import json
-    import requests
-
     res = requests.get("https://randomuser.me/api/")
-    res=res.json()
-    res = res['results'][0]
+    res = res.json()
+    return res['results'][0]
 
-    return res
 
 def format_data(res):
     data = {}
@@ -26,44 +26,37 @@ def format_data(res):
     data['last_name'] = res['name']['last']
     data['gender'] = res['gender']
     location = res['location']
-    data['address'] = f"{str(location['street']['number'])} {location['street']['name']}"\
-                        f"{location['city']}, {location['state']}, {location['country']}"
+    data['address'] = (
+        f"{location['street']['number']} {location['street']['name']}, "
+        f"{location['city']}, {location['state']}, {location['country']}"
+    )
     data['postcode'] = location['postcode']
     data['email'] = res['email']
     data['username'] = res['login']['username']
     data['dob'] = res['dob']
     data['registered_date'] = res['registered']['date']
-    data['phone'] =res['phone']
+    data['phone'] = res['phone']
     data['picture'] = res['picture']['medium']
-
     return data
 
 
-
 def stream_data():
-    from kafka import KafkaProducer
-    import json
-    import time
-
     res = get_data()
     res = format_data(res)
-    # print(json.dumps(res, indent=3))
-
-    producer = KafkaProducer(bootstrap_servers = ['broker:29092'], max_block_ms=5000)
-    # publish data to the queue
+    producer = KafkaProducer(
+        bootstrap_servers=['broker:29092'],
+        max_block_ms=5000
+    )
     producer.send('users_created', json.dumps(res).encode('utf-8'))
 
 
-with DAG('user_automation',
-            default_args=default_args,
-            schedule_interval='@daily',
-            catchup=False
-         
-         ) as dag:
+with DAG(
+    'user_automation',
+    default_args=default_args,
+    schedule_interval='@daily',
+    catchup=False
+) as dag:
     streaming_task = PythonOperator(
-        task_id = 'stream_data_from_api',
-        python_callable=stream_data,
+        task_id='stream_data_from_api',
+        python_callable=stream_data
     )
-    
-
-# stream_data()
